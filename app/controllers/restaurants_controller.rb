574015fbf5203db31c5b53e3c261d5d3c5c17eb9
AlexIrungu <aleaxmuiruri@gmail.com>
2023-03-20 13:00:20 +0300

    configured routes

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,8 @@
 class RestaurantsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+    def index
+        rest = Restaurant.all
+        render json: rest
+    end
 end
